06fbdfd5fc64e5cfd87adc0050c948b453c7f752
Azad Yaşar <ceazadyasar@gmail.com>
2021-02-20 15:55:03 +0300

    Add dataset and required vocabulary objects

--- src/nmt/__main__.py
+++ src/nmt/__main__.py
@@ -1,13 +1,13 @@
 import argparse
-from nmt import (train_model)
+from nmt import train_model
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(
-    prog='NMT',
+    prog='nmt',
     description='PyTorch implementation of GRU+Attention and the Transformer NMT models')
   subparsers = parser.add_subparsers(dest='subcommands')
   
   train_model.add_subparser(subparsers)
   
   args = parser.parse_args()
-  argparse.func(args)
+  args.func(args)
